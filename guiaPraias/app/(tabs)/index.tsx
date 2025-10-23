af47141baf28070e9b5876b39b91dbd5d5aabe5b
Mista-404 <alanfrancasilva2077@outlook.com>
2025-10-23 17:56:09 +0000

    Versão 0.2

--- guiaPraias/app/(tabs)/index.tsx
+++ guiaPraias/app/(tabs)/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import {StyleSheet, View, Text, Image, TouchableOpacity, SafeAreaView, StatusBar} from 'react-native';
 
-export function home() {
+const CategoryCard = ({backgroundColor, title, titleColor = '#fff'}) =>
+(
+  <TouchableOpacity style={[styles.card, {backgroundColor}]}>
+    <text style={[styles.cardText, {color: titleColor}]}></text>
+  </TouchableOpacity>
+);
+
+export default  function home() {
   return(
-    <SafeAreaView Style={styles.container}>
-    <StatusBar barStyle="dark-content" />
-    <Image source={require('./assets/praia.jpg')} style={styles.headerImage} />
+    <SafeAreaView style={styles.container}>
+    <StatusBar barstyle="dark-content" />
+    <Image
+    source={require('../assets/praia.jpg')}
+    style={styles.headerImage} />
     <View style={styles.titleContainer}>
       <Text style={styles.mainTitle}>Guia de Praias</Text>
       <Text style={styles.subTitle}>Planeje sua Viagem</Text>
@@ -17,6 +26,61 @@ export function home() {
 const styles=StyleSheet.create({
   container:{
     flex:1,
-    backgroundColor:'#f0f4f8',
-    alignItems:'center',},
+    backgroundColor:'#fff',
+    alignItems:'center',
+  },
+
+  headerImage:{
+    width: '100%',
+    height: 250,
+    resizeMode: 'cover',
+  },
+
+  titleContainer:{
+    marginVertical: 24,
+    alignItems: 'center',
+  },
+
+  mainTitle:{
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#5743EC',
+  },
+
+  subTitle:{
+    fontSize: 16,
+    color: '#2E21B8',
+    marginTop: 4,
+  },
+
+  grid:{
+    width: '90%',
+  },
+
+  row:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 16,
+  },
+
+  card:{
+    width: '48%',
+    height: 120,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.85,
+    elevation: 5,
+  },
+
+  cardText:{
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
 });
